803930fb40de4237d2d84b015241710da12e6b46
Kevin Paul <kpaul@ucar.edu>
2018-12-28 13:23:38 -0700

    DRYing up testsˆ

--- mpipytest/tests/test_serialization.py
+++ mpipytest/tests/test_serialization.py
@@ -1,31 +1,60 @@
 import sys
+import pytest
 
 from mpipytest.serialization import serialize_test, deserialize_test
 
 
-def test_serialize_test_type():
-    def func(*arguments, **keywords):
-        return arguments, keywords
+@pytest.fixture
+def global_func():
+    def func(*args, **kwargs):
+        return args, kwargs
+    return func
 
-    serialized_test = serialize_test(func, 1, 'a', x=2, y='b')
 
+@pytest.fixture
+def global_func_args():
+    return 1, 'a'
+
+
+@pytest.fixture
+def global_func_kwargs():
+    return {'x': 2, 'y': 'b'}
+
+
+@pytest.fixture
+def serialize_global_func(global_func, global_func_args, global_func_kwargs):
+    return serialize_test(global_func, *global_func_args, **global_func_kwargs)
+
+
+@pytest.fixture
+def deserialize_global_func(serialize_global_func):
+    return deserialize_test(serialize_global_func)
+
+
+def test_serialize_test(serialize_global_func):
     if sys.version_info[0] == 2:
-        assert isinstance(serialized_test, str)
+        assert isinstance(serialize_global_func, str)
     elif sys.version_info[0] == 3:
-        assert isinstance(serialized_test, bytes)
+        assert isinstance(serialize_global_func, bytes)
 
 
-def test_deserialize_test():
-    def func(*arguments, **keywords):
-        return arguments, keywords
+def test_deserialize_test_global(global_func, global_func_args, global_func_kwargs, deserialize_global_func):
+    d_func, d_args, d_kwargs = deserialize_global_func
 
-    args = (1, 'a')
-    kwargs = {'x': 2, 'y': 'b'}
+    assert d_func.__name__ == global_func.__name__
+    assert d_args == global_func_args
+    assert d_kwargs == global_func_kwargs
+    assert d_func(*d_args, **d_kwargs) == global_func(*global_func_args, **global_func_kwargs)
 
-    serialized_test = serialize_test(func, *args, **kwargs)
+
+def test_deserialize_test_local(global_func_args, global_func_kwargs):
+    def local_func(*args, **kwargs):
+        return args, kwargs
+
+    serialized_test = serialize_test(local_func, *global_func_args, **global_func_kwargs)
     d_func, d_args, d_kwargs = deserialize_test(serialized_test)
 
-    assert d_func.__name__ == func.__name__
-    assert d_args == args
-    assert d_kwargs == kwargs
-    assert d_func(*d_args, **d_kwargs) == func(*args, **kwargs)
+    assert d_func.__name__ == local_func.__name__
+    assert d_args == global_func_args
+    assert d_kwargs == global_func_kwargs
+    assert d_func(*d_args, **d_kwargs) == local_func(*global_func_args, **global_func_kwargs)
