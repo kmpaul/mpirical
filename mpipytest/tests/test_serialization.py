a7612569db16270255722899d0003a34028f0cf3
Kevin Paul <kpaul@ucar.edu>
2018-12-28 14:03:14 -0700

    Encapsulating serializable tests in new class

--- mpipytest/tests/test_serialization.py
+++ mpipytest/tests/test_serialization.py
@@ -1,60 +1,75 @@
 import sys
 import pytest
+import pickle
 
-from mpipytest.serialization import serialize_test, deserialize_test
+from mpipytest.serialization import serialize_test, deserialize_test, SerializableTest
 
 
 @pytest.fixture
-def global_func():
+def func_fixture():
     def func(*args, **kwargs):
         return args, kwargs
     return func
 
 
 @pytest.fixture
-def global_func_args():
+def args_fixture():
     return 1, 'a'
 
 
 @pytest.fixture
-def global_func_kwargs():
+def kwargs_fixture():
     return {'x': 2, 'y': 'b'}
 
 
 @pytest.fixture
-def serialize_global_func(global_func, global_func_args, global_func_kwargs):
-    return serialize_test(global_func, *global_func_args, **global_func_kwargs)
+def serializable_fixture(func_fixture, args_fixture, kwargs_fixture):
+    return SerializableTest(func_fixture, *args_fixture, **kwargs_fixture)
 
 
 @pytest.fixture
-def deserialize_global_func(serialize_global_func):
-    return deserialize_test(serialize_global_func)
+def serialized_test(func_fixture, args_fixture, kwargs_fixture):
+    return serialize_test(func_fixture, *args_fixture, **kwargs_fixture)
 
 
-def test_serialize_test(serialize_global_func):
+@pytest.fixture
+def deserialized_test(serialized_test):
+    return deserialize_test(serialized_test)
+
+
+def test_serialize_test(serialized_test):
     if sys.version_info[0] == 2:
-        assert isinstance(serialize_global_func, str)
+        assert isinstance(serialized_test, str)
     elif sys.version_info[0] == 3:
-        assert isinstance(serialize_global_func, bytes)
+        assert isinstance(serialized_test, bytes)
 
 
-def test_deserialize_test_global(global_func, global_func_args, global_func_kwargs, deserialize_global_func):
-    d_func, d_args, d_kwargs = deserialize_global_func
+def test_deserialize_test(func_fixture, args_fixture, kwargs_fixture, deserialized_test):
+    d_func, d_args, d_kwargs = deserialized_test
 
-    assert d_func.__name__ == global_func.__name__
-    assert d_args == global_func_args
-    assert d_kwargs == global_func_kwargs
-    assert d_func(*d_args, **d_kwargs) == global_func(*global_func_args, **global_func_kwargs)
+    assert d_func.__name__ == func_fixture.__name__
+    assert d_args == args_fixture
+    assert d_kwargs == kwargs_fixture
+    assert d_func(*d_args, **d_kwargs) == func_fixture(*args_fixture, **kwargs_fixture)
 
 
-def test_deserialize_test_local(global_func_args, global_func_kwargs):
-    def local_func(*args, **kwargs):
-        return args, kwargs
+def test_serialize_serializable_test(serializable_fixture):
+    serialized_test = serializable_fixture.serialize()
+    if sys.version_info[0] == 2:
+        assert isinstance(serialized_test, str)
+    elif sys.version_info[0] == 3:
+        assert isinstance(serialized_test, bytes)
 
-    serialized_test = serialize_test(local_func, *global_func_args, **global_func_kwargs)
-    d_func, d_args, d_kwargs = deserialize_test(serialized_test)
 
-    assert d_func.__name__ == local_func.__name__
-    assert d_args == global_func_args
-    assert d_kwargs == global_func_kwargs
-    assert d_func(*d_args, **d_kwargs) == local_func(*global_func_args, **global_func_kwargs)
+def test_deserialize_serializable_test(func_fixture, args_fixture, kwargs_fixture, serializable_fixture):
+    d_test = SerializableTest.deserialize(serializable_fixture.serialize())
+
+    assert d_test.func.__name__ == func_fixture.__name__
+    assert d_test.args == args_fixture
+    assert d_test.kwargs == kwargs_fixture
+    assert d_test.run() == func_fixture(*args_fixture, **kwargs_fixture)
+
+
+def test_deserialize_nontest():
+    s_obj = pickle.dumps((1, 'a', {'b': 3}))
+    nontest = SerializableTest.deserialize(s_obj)
