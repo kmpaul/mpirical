7302ce2e9e8048c27a09d5b1a24fd62cbbe6b9a5
Kevin Paul <kpaul@ucar.edu>
2018-12-28 09:30:42 -0700

    More helpful output

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -3,14 +3,28 @@
 set -e
 set -eo pipefail
 
+echo "===== UPDATE CONTAINER ====="; echo
 apt-get update; apt-get install -y make
+
+echo "===== SETUP CONDA ====="; echo
 conda config --set always_yes true --set changeps1 false --set quiet true
 conda update --quiet conda
 conda config --add channels conda-forge
+
+echo "===== CREATE ${ENV_NAME} ENVIRONMENT WITH PYTHON ${PYTHON}====="; echo
 conda env create --name ${ENV_NAME} python=${PYTHON} --quiet
-conda env list
+
+echo "===== ACTIVATE ${ENV_NAME} ENVIRONMENT ====="; echo
 source activate ${ENV_NAME}
+
+echo "===== UPDATE ${ENV_NAME} ENVIRONMENT ====="; echo
 conda env update --file environment-dev.yml --quiet
+
+echo "===== UPDATE PIP ====="; echo
 pip install pip --upgrade
+
+echo "===== INSTALL PACKAGE ====="; echo
 pip install --no-deps --quiet -e .
-conda list --name ${ENV_NAME}
+
+echo "===== CONDA LIST ${ENV_NAME} ====="; echo
+conda list
