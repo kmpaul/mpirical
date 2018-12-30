702949e2307cdd4da87c89f8f999951f8996a3cd
Kevin Paul <kpaul@ucar.edu>
2018-12-30 10:31:21 -0700

    Move fixtures and test functions into utils

--- mpipytest/tests/test_serialization.py
+++ mpipytest/tests/test_serialization.py
@@ -4,18 +4,7 @@ import dill
 import cloudpickle
 
 from serialization import serialize, deserialize
-
-
-def mpi_bcast(val):
-    from mpi4py import MPI
-    x_rank = val if MPI.COMM_WORLD.Get_rank() == 0 else None
-    x_all = MPI.COMM_WORLD.bcast(x_rank, root=0)
-    return x_all
-
-
-@pytest.fixture
-def serialized_mpi_bcast():
-    return serialize(mpi_bcast)
+from tests.utils import mpi_bcast, serialized_mpi_bcast
 
 
 @pytest.mark.parametrize('serializers', ([dill], [pickle], [cloudpickle]))
