4d98f7cc96fee88aced21bec1598c84a96d4c347
Kevin Paul <kpaul@ucar.edu>
2018-12-30 10:04:23 -0700

    Add deserialize function and tests

--- mpipytest/serialization.py
+++ mpipytest/serialization.py
@@ -20,3 +20,22 @@ def serialize(obj, *args, **kwargs):
         raise last_exception
     else:
         return serialized_obj
+
+
+def deserialize(serialized_obj, *args, **kwargs):
+    deserializers = kwargs.pop('deserializers', [dill, cloudpickle, pickle])
+    unsuccessful = True
+    last_exception = None
+    deserialized_obj = None
+    while deserializers and unsuccessful:
+        deserializer = deserializers.pop(0)
+        try:
+            deserialized_obj = deserializer.loads(serialized_obj, *args, **kwargs)
+        except Exception as e:
+            last_exception = e
+        else:
+            unsuccessful = False
+    if unsuccessful and last_exception is not None:
+        raise last_exception
+    else:
+        return deserialized_obj
--- mpipytest/tests/test_serialization.py
+++ mpipytest/tests/test_serialization.py
@@ -3,18 +3,27 @@ import pickle
 import dill
 import cloudpickle
 
-from serialization import serialize
+from serialization import serialize, deserialize
 
 
 def mpi_bcast(val):
     from mpi4py import MPI
-
     x_rank = val if MPI.COMM_WORLD.Get_rank() == 0 else None
     x_all = MPI.COMM_WORLD.bcast(x_rank, root=0)
-
     return x_all
 
 
+@pytest.fixture
+def serialized_mpi_bcast():
+    return serialize(mpi_bcast)
+
+
 @pytest.mark.parametrize('serializers', ([dill], [pickle], [cloudpickle]))
-def test_serialize_mpi_bcast_cloudpickle(serializers):
+def test_serialize_mpi_bcast(serializers):
     serialize(mpi_bcast, serializers=serializers)
+
+
+@pytest.mark.parametrize('deserializers', ([dill], [pickle], [cloudpickle]))
+def test_deserialize_mpi_bcast(deserializers, serialized_mpi_bcast):
+    deserialized_mpi_bcast = deserialize(serialized_mpi_bcast, deserializers=deserializers)
+    assert deserialized_mpi_bcast('x') == 'x'
