80df8ecd22226b266b582907b6d65e46972a54ff
Kevin Paul <kpaul@ucar.edu>
2018-12-31 09:04:01 -0700

    Adding tasks and task tests

--- mpipytest/tasks.py
+++ mpipytest/tasks.py
@@ -2,4 +2,11 @@ class Task(object):
     """A serializable object containing a function and arguments"""
 
     def __init__(self, func, *args, **kwargs):
-        pass
+        if not callable(func):
+            raise ValueError('Task function not callable')
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    def compute(self):
+        return self.func(*self.args, **self.kwargs)
--- mpipytest/tests/test_tasks.py
+++ mpipytest/tests/test_tasks.py
@@ -1,15 +1,66 @@
 import pytest
 
 from mpipytest.tests.utils import mpi_bcast
+from mpipytest.serialization import serialize, deserialize
 from mpipytest.tasks import Task
 
 
-def test_task_init():
+def main_test_func(*args, **kwargs):
+    return args, kwargs
+
+
+def test_main_func_task():
+    task = Task(main_test_func, 1, 'a', x='y')
+    assert task.compute() == main_test_func(1, 'a', x='y')
+
+
+def test_local_func_task():
+    def local_test_func(x, y=3):
+        return x, y
+    task = Task(local_test_func, 4)
+    assert task.compute() == local_test_func(4)
+
+
+def test_mpi_bcast_task():
     task = Task(mpi_bcast, 'x')
-    assert isinstance(task, Task)
+    assert task.compute() == mpi_bcast('x')
+
+
+def test_lambda_task():
+    task = Task(lambda x: x, 1)
+    assert task.compute() == 1
 
 
 def test_task_func_not_callable_raises_exception():
     with pytest.raises(ValueError):
         Task(1, 'x')
 
+
+def test_serialize_main_task():
+    task = Task(main_test_func, 1, 'a', x='y')
+    serialized_task = serialize(task)
+    deserialized_task = deserialize(serialized_task)
+    assert deserialized_task.compute() == task.compute()
+
+
+def test_serialize_local_task():
+    def local_test_func(x, y=3):
+        return x, y
+    task = Task(local_test_func, 4)
+    serialized_task = serialize(task)
+    deserialized_task = deserialize(serialized_task)
+    assert deserialized_task.compute() == task.compute()
+
+
+def test_serialize_mpi_bcast_task():
+    task = Task(mpi_bcast, 'x')
+    serialized_task = serialize(task)
+    deserialized_task = deserialize(serialized_task)
+    assert deserialized_task.compute() == task.compute()
+
+
+def test_serialize_lambda_task():
+    task = Task(lambda x: x, 1)
+    serialized_task = serialize(task)
+    deserialized_task = deserialize(serialized_task)
+    assert deserialized_task.compute() == task.compute()
