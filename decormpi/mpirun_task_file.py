689c1fececcb7783b1d6f3d64c45eb069f06562b
Kevin Paul <kpaul@ucar.edu>
2018-12-31 16:22:14 -0700

    Functions to aid in running MPI tasks

---
+++ decormpi/mpirun_task_file.py
@@ -0,0 +1,37 @@
+import mpi4py
+
+from os.path import realpath, dirname, join, exists
+from sys import executable, argv
+from subprocess import Popen
+from decormpi.serialization import serialize, deserialize
+
+THIS_SCRIPT = realpath(__file__)
+MPIRUN = join(dirname(mpi4py.get_config()['mpicc']), 'mpirun')
+if not exists(MPIRUN):
+    raise RuntimeError('Cannot find mpirun')
+
+
+def subprocess_mpirun_task_file(task_file, result_file=None, nprocs=1, timeout=10):
+    if result_file is None:
+        result_file = '{}.result'.format(task_file)
+    p = Popen([MPIRUN, '-np', str(nprocs), executable, THIS_SCRIPT, task_file, result_file])
+    p.communicate(timeout=timeout)
+    if p.returncode != 0:
+        raise RuntimeError('Task failed to run')
+
+
+def mpirun_task_file(task_file, result_file=None):
+    if result_file is None:
+        result_file = '{}.result'.format(task_file)
+    from mpi4py import MPI
+    task = deserialize(file=task_file)
+    result = task.compute()
+    results = MPI.COMM_WORLD.gather(result)
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        serialize(results, file=result_file)
+
+
+if __name__ == '__main__':
+    t_file = argv[1]
+    r_file = argv[2] if len(argv) > 2 else None
+    mpirun_task_file(t_file, result_file=r_file)
---
+++ decormpi/tests/test_mpirun_task_file.py
@@ -0,0 +1,45 @@
+import os
+
+from decormpi.tasks import Task
+from decormpi.serialization import serialize, deserialize
+from decormpi.mpirun_task_file import subprocess_mpirun_task_file, mpirun_task_file
+
+
+def main_test_func(x, y=2):
+    return x, y
+
+
+def test_mpirun_task_file_serial():
+    task_file = 'main_test_func.task'
+    task = Task(main_test_func, 1)
+    serialize(task, file=task_file)
+
+    mpirun_task_file(task_file)
+    result_file = task_file + '.result'
+    assert os.path.exists(result_file)
+
+    results = deserialize(file=result_file)
+    assert results == [(1, 2)]
+
+    if os.path.exists(task_file):
+        os.remove(task_file)
+    if os.path.exists(result_file):
+        os.remove(result_file)
+
+
+def test_subprocess_mpirun_task_file():
+    task_file = 'main_test_func.task'
+    task = Task(main_test_func, 1)
+    serialize(task, file=task_file)
+
+    subprocess_mpirun_task_file(task_file, nprocs=2)
+    result_file = task_file + '.result'
+    assert os.path.exists(result_file)
+
+    results = deserialize(file=result_file)
+    assert results == [(1, 2), (1, 2)]
+
+    if os.path.exists(task_file):
+        os.remove(task_file)
+    if os.path.exists(result_file):
+        os.remove(result_file)
