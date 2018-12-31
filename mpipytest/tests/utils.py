1feb99466ab00e02b1fd92479f4e3b8915695c7d
Kevin Paul <kpaul@ucar.edu>
2018-12-31 09:03:45 -0700

    Adding utility module to store common functions

--- mpipytest/tests/utils.py
+++ mpipytest/tests/utils.py
@@ -1,15 +1,5 @@
-import pytest
-
-from serialization import serialize
-
-
 def mpi_bcast(val):
     from mpi4py import MPI
     x_rank = val if MPI.COMM_WORLD.Get_rank() == 0 else None
     x_all = MPI.COMM_WORLD.bcast(x_rank, root=0)
     return x_all
-
-
-@pytest.fixture
-def serialized_mpi_bcast():
-    return serialize(mpi_bcast)
