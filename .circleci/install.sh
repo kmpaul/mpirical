5aaa707173dafe8b70b7e966796c9d62dbb27912
Kevin Paul <kpaul@ucar.edu>
2019-02-13 17:00:44 -0700

    Force name of conda environment

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -7,7 +7,7 @@ conda config --set always_yes true --set quiet true
 conda update conda
 conda config --set pip_interop_enabled True # Enable pip interoperability
 conda config --add channels conda-forge
-conda env create -f environment-dev.yml
+conda env create -f environment-dev.yml --name ${ENV_NAME}
 conda env list
 source activate ${ENV_NAME}
 pip install pip --upgrade
