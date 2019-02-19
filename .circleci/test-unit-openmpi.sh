5ae8620b53756e52bde8188c4d8950ce4a026011
Kevin Paul <kpaul@ucar.edu>
2019-02-19 14:27:03 -0700

    Break up mpiruntests

--- .circleci/test-unit-openmpi.sh
+++ .circleci/test-unit-openmpi.sh
@@ -4,4 +4,5 @@ echo "[Running Tests]"
 pytest --junitxml=test-reports/junit.xml mpirical/tests/test_tasks.py
 pytest --junitxml=test-reports/junit.xml mpirical/tests/test_decorator.py
 pytest --junitxml=test-reports/junit.xml mpirical/tests/test_serialization.py
-pytest --junitxml=test-reports/junit.xml mpirical/tests/test_mpiruntask.py
+pytest --junitxml=test-reports/junit.xml mpirical/tests/test_mpiruntask.py::test_mpirun_task_file_serial
+pytest --junitxml=test-reports/junit.xml mpirical/tests/test_mpiruntask.py::test_mpirun_task_file_parallel
