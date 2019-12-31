2036bfb12d3fbc8bd1bd3c0f9805c73925e17092
Kevin Paul <kpaul@ucar.edu>
2019-12-31 20:45:52 +0000

    Modify tests to match simplification

--- mpirical/tests/test_serialization.py
+++ mpirical/tests/test_serialization.py
@@ -9,7 +9,7 @@ from mpirical.serialization import serialize, deserialize
 from mpirical.tests.utils import mpi_bcast
 
 
-def test_serialize_mpi_bcast_default():
+def test_serialize_mpi_bcast():
     serialized_mpi_bcast = serialize(mpi_bcast)
     if sys.version_info[0] == 2:
         assert isinstance(serialized_mpi_bcast, str)
@@ -17,26 +17,9 @@ def test_serialize_mpi_bcast_default():
         assert isinstance(serialized_mpi_bcast, bytes)
 
 
-@pytest.mark.parametrize('serializers', ([dill], [pickle], [cloudpickle]))
-def test_serialize_mpi_bcast(serializers):
-    serialized_mpi_bcast = serialize(mpi_bcast, serializers=serializers)
-    if sys.version_info[0] == 2:
-        assert isinstance(serialized_mpi_bcast, str)
-    elif sys.version_info[0] == 3:
-        assert isinstance(serialized_mpi_bcast, bytes)
-
-
-def test_deserialize_mpi_bcast_default():
+def test_deserialize_mpi_bcast():
     serialized_mpi_bcast = serialize(mpi_bcast)
     deserialized_mpi_bcast = deserialize(serialized_mpi_bcast)
-    assert deserialized_mpi_bcast('x') == mpi_bcast('x')
-
-
-@pytest.mark.parametrize('deserializers', ([dill], [pickle], [cloudpickle]))
-def test_deserialize_mpi_bcast(deserializers):
-    serialized_mpi_bcast = serialize(mpi_bcast, serializers=deserializers)
-    deserialized_mpi_bcast = deserialize(serialized_mpi_bcast,
-                                         deserializers=deserializers)
     assert deserialized_mpi_bcast('x') == mpi_bcast('x')
 
 
