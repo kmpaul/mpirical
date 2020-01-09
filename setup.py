afb4443dc6c7da23287147908fd7e4ff9894497a
Julia Kent <jkent@ucar.edu>
2020-01-09 11:37:37 -0700

    Install setuptools_scm

--- setup.py
+++ setup.py
@@ -22,13 +22,15 @@ with open('README.rst') as f:
 
 
 setup(name='mpirical',
-      description='Decorate any function to make it run in parallel with MPI',
-      url='https://github.com/NCAR/mpirical',
-      maintainer='Kevin Paul',
-      maintainer_email='kpaul@ucar.edu',
-      license='https://www.apache.org/licenses/LICENSE-2.0',
-      long_description=long_description,
-      include_package_data=True,
-      install_requires=install_requires,
-      packages=['mpirical'],
-      zip_safe=False)
+    description='Decorate any function to make it run in parallel with MPI',
+    url='https://github.com/NCAR/mpirical',
+    maintainer='Kevin Paul',
+    maintainer_email='kpaul@ucar.edu',
+    license='https://www.apache.org/licenses/LICENSE-2.0',
+    long_description=long_description,
+    include_package_data=True,
+    install_requires=install_requires,
+    packages=['mpirical'],
+    zip_safe=False,
+    use_scm_version={'version_scheme': 'post-release', 'local_scheme': 'dirty-tag'},
+    setup_requires=['setuptools_scm', 'setuptools>=30.3.0'])
